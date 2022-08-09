2c1e9049ef99437cd29b79e16363df41aa8cff9c
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2022-08-09 03:29:05 -0700

    changes

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,66 +1,33 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  SpyToken,
-  Approval,
-  OwnershipTransferred,
   Transfer
 } from "../generated/SpyToken/SpyToken"
-import { ExampleEntity } from "../generated/schema"
+import { Burn, Info } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+let DEAD_ADDRESS = "0x000000000000000000000000000000000000dEaD"
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+export function handleTransfer(event: Transfer): void {
+  if (event.params.to.toHex().toLowerCase() == DEAD_ADDRESS.toLowerCase()) {
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    let info = Info.load("info-base");
+    if (!info) {
+      info = new Info("info-base");
+      info.totalAmount = event.params.value;
+      info.count = 1;
+      info.save();
+    } else {
+      info.totalAmount = info.totalAmount.plus(event.params.value);
+      info.count ++;
+      info.save();
+    }
+
+    let burnId = "burn-".concat(event.transaction.hash.toHex())
+    let burn = new Burn(burnId)
+    burn.index = info.count - 1;
+    burn.amount = event.params.value
+    burn.from = event.params.from
+    burn.creationTime = event.block.timestamp
+    burn.txid = event.transaction.hash
+    burn.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.getOwner(...)
-  // - contract.increaseAllowance(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
 }
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransfer(event: Transfer): void {}
